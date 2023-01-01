fb281e16a26d046078dc5e0ec81b9bc439c4831a
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 11:45:51 +0300

    some modifications

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,18 +16,18 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $recent_news_data = Post::orderBy('id','desc')->get();
-        $popular_news_data = Post::orderBy('visitors','desc')->get();
+    //     $recent_news_data = Post::orderBy('id','desc')->get();
+    //     $popular_news_data = Post::orderBy('visitors','desc')->get();
        
 
-        $catagories_data = Catagory::with('subCatagories')
-        ->where('show_on_menu','show')
-        ->whereHas('subCatagories',function($query){
-           $query->where('show_on_menu','show');
-     })->get();
+    //     $catagories_data = Catagory::with('subCatagories')
+    //     ->where('show_on_menu','show')
+    //     ->whereHas('subCatagories',function($query){
+    //        $query->where('show_on_menu','show');
+    //  })->get();
 
-        view()->share('global_recent_news_data',$recent_news_data);
-        view()->share('global_popular_news_data',$popular_news_data);
-        view()->share('global_catagories',$catagories_data);
+    //     view()->share('global_recent_news_data',$recent_news_data);
+    //     view()->share('global_popular_news_data',$popular_news_data);
+    //     view()->share('global_catagories',$catagories_data);
     }
 }
