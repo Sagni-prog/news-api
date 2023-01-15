dee8feff09f6d197213f335ecc06e96a5a94031d
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-15 16:43:25 +0300

    profile photo update done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -89,7 +89,6 @@ class AdminController extends Controller
     }
 
     public function logout(){
-        // Auth::logout();
         
         Auth::user()->tokens()->delete();
 
@@ -99,12 +98,41 @@ class AdminController extends Controller
   
     public function updateProfile(Request $request){
 
-          $user =  Auth::user()->update([
+        $user = Auth::user()->first();
+
+          $isUpdated =  Auth::user()->update([
                 "name" => $request->name,
                 "email" => $request->email
             ]);
 
-            return Auth::user()->with('photo')->first();
+            if($request->hasFile('photo')){
+            
+                $ext = $request->file('photo')->extension();
+                
+                $image_name = 'image';
+               
+                
+                $filename = 'image-' . time() . '.' . $ext;
+            
+             $path = $request->file('photo')->storeAs('profile-photo', $filename);
+             $image_url = Storage::url($path);
+      
+             $data = $this->getDimension($path);
+             $width = $data['width'];
+             $height = $data['height'];
+
+            
+          $user->photo()->create([
+                "photo_name" => $filename,
+                "photo_path" => $path,
+                "photo_url" => $image_url,
+                "photo_width" => $width,
+                "photo_height" => $height
+            ]);
+
+       }
+
+            return $user;
      }
 
     public function updatePassword(Request $request){
@@ -113,8 +141,11 @@ class AdminController extends Controller
                 "password" => Hash::make($request->password),
             ]);
 
-            return $user;
-     }
+            if($user){
+
+            return Auth::user()->with('photo')->first();
+       }
+    }
 
     public static function getDimension($path){
         [$width,$height] = getimagesize(Storage::path($path));
